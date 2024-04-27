6e4fa18496ba2d218e119690482140ecbf168bb9
denux209 <147421147+denux209@users.noreply.github.com>
2024-04-27 12:52:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+# İçe Aktarma
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    # Elektrikli cihazların enerji tüketimini hesaplamaya olanak tanıyan değişkenler
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+# İlk sayfa
+@app.route('/')
+def index():
+    return render_template('index.html')
+# İkinci sayfa
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+# Üçüncü sayfa
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',                           
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Hesaplama
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(float(size),
+                                                    float(lights), 
+                                                    float(device)
+                                                    )
+                        )
+# Form
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Formun sonuçları
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    # Veri toplama için değişkenleri tanımlayın
+    name = request.form['name']
+    email = request.form.get("email")
+    adress =request.form.get("adress") 
+    date = request.form.get("date")
+    # Verilerinizi kaydedebilir veya e-posta ile gönderebilirsiniz
+    with open("dosyaadi.txt","a",encoding="UTF-8") as notdefteri:
+        notdefteri.write(f"isim ={name}\n")
+        notdefteri.write(f"mail ={email}\n")
+        notdefteri.write(f"adres ={adress}\n")
+        notdefteri.write(f"tarih ={date}\n")
+        notdefteri.write(f"---------------------------------------------\n")
+    return render_template('form_result.html', 
+                           # Değişkenleri buraya yerleştirin
+                           name=name,
+                           email=email,
+                           adress=adress,
+                           date=date
+                           )
+
+app.run(debug=True)
